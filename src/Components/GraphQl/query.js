71a5da982c44187a4a697e335b369305fc7a08cb
abbas ejlali <abbas.ejlali18@gmail.com>
2023-04-27 06:06:31 -0700

    add home and header to app

--- src/Components/GraphQl/query.js
+++ src/Components/GraphQl/query.js
@@ -1,12 +1,3 @@
 import { gql } from "@apollo/client";
 
-const GET_AUTHOR = gql`
-  query {
-    authors {
-      id
-      name
-    }
-  }
-`;
-
 export { GET_AUTHOR };
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Components
+import Header from "../Layout/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
