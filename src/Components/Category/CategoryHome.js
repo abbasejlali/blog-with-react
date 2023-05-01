ab9eaf2d63f61c4514f1dedcf015405a3c0f1300
abbas ejlali <abbas.ejlali18@gmail.com>
2023-05-01 09:36:06 -0700

    add jsx and styles to categoryhome

--- src/Components/Category/CategoryHome.js
+++ src/Components/Category/CategoryHome.js
@@ -1,7 +1,137 @@
 import React from "react";
 
+// Mui
+import { Box, Typography, Grid } from "@mui/material";
+
+// styles
+import styles from "./CategoryHome.module.css";
+
+// react-router-dom
+import { Link } from "react-router-dom";
+
 const CategoryHome = () => {
-  return <div>CategoryHome</div>;
+  return (
+    <Box
+      px={4.5}
+      mb={5}
+      xs={12}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+      }}
+    >
+      <Typography
+        component="h3"
+        fontWeight="bold"
+        xs={12}
+        mb={3}
+        variant="h5"
+        color="#666"
+      >
+        دسته بندی
+      </Typography>
+      <Grid
+        container
+        xs={12}
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+        className={styles.grid_categoryhome}
+      >
+        <Grid
+          item
+          xs={12}
+          sm={5.8}
+          md={3.8}
+          height="90px"
+          sx={{
+            backgroundColor: "rgba(0, 0, 0, 0.04) !important",
+            borderRadius: "10px",
+          }}
+        >
+          <Link to="/">
+            <Typography
+              component="h4"
+              variant="h6"
+              fontWeight="bold"
+              color="#666"
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              width="100%"
+              height="100%"
+            >
+              برنامه نویسی
+            </Typography>
+          </Link>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={5.8}
+          md={3.8}
+          height="90px"
+          sx={{
+            backgroundColor: "rgba(0, 0, 0, 0.04) !important",
+            borderRadius: "10px",
+          }}
+        >
+          <Link to="/">
+            <Typography
+              component="h4"
+              variant="h6"
+              color="#666"
+              fontWeight="bold"
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              width="100%"
+              height="100%"
+            >
+              دنیای دیجیتال
+            </Typography>
+          </Link>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={5.8}
+          md={3.8}
+          height="90px"
+          sx={{
+            backgroundColor: "rgba(0, 0, 0, 0.04) !important",
+            borderRadius: "10px",
+          }}
+        >
+          <Link to="/">
+            <Typography
+              component="h4"
+              variant="h6"
+              color="#666"
+              fontWeight="bold"
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              width="100%"
+              height="100%"
+            >
+              تکنولوژی
+            </Typography>
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default CategoryHome;
