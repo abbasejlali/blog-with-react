4224f15a7b27799dad426421a7df1bb1b8356fda
abbas ejlali <abbas.ejlali18@gmail.com>
2023-05-01 08:07:52 -0700

    biuld and add categoryhome component to project and home.js

---
+++ src/Components/Category/CategoryHome.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CategoryHome = () => {
+  return <div>CategoryHome</div>;
+};
+
+export default CategoryHome;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,12 +3,14 @@ import React from "react";
 // Components
 import Header from "../Layout/Header";
 import SlideShow from "../Slider/SlideShow";
+import CategoryHome from "../Category/CategoryHome";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <SlideShow />
+      <CategoryHome />
     </div>
   );
 };
