02e35b5ba09d6436f3169f3ca158f2899af5cd3b
abbas ejlali <abbas.ejlali18@gmail.com>
2023-05-22 07:13:02 -0700

    build motion in categoryHome.js

--- src/Components/Category/CategoryHome.js
+++ src/Components/Category/CategoryHome.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 // Mui
 import { Box, Typography, Grid } from "@mui/material";
@@ -9,7 +9,65 @@ import styles from "./CategoryHome.module.css";
 // react-router-dom
 import { Link } from "react-router-dom";
 
+// Green Sock
+import { gsap } from "gsap";
+
 const CategoryHome = () => {
+  const Itemone = useRef();
+  const Itemtwo = useRef();
+  const Itemthree = useRef();
+  const tl = useRef();
+  const App1 = useRef();
+
+  useEffect(() => {
+    tl.current = gsap
+      .timeline()
+      .fromTo(
+        Itemone.current,
+        {
+          y: 50,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.35,
+          scrollTrigger: {
+            trigger: "#boxcategory",
+            start: "top center",
+          },
+        }
+      )
+      .fromTo(
+        Itemtwo.current,
+        {
+          y: 50,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.35,
+          scrollTrigger: {
+            trigger: "#boxcategory",
+            start: "top center",
+          },
+        }
+      )
+      .fromTo(
+        Itemthree.current,
+        {
+          y: 50,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.35,
+          scrollTrigger: {
+            trigger: "#boxcategory",
+            start: "top center",
+          },
+        }
+      );
+  }, []);
   return (
     <Box
       mb={5}
@@ -21,6 +79,8 @@ const CategoryHome = () => {
         alignItems: "flex-start",
       }}
       className={styles.boxcategory}
+      id="boxcategory"
+      ref={App1}
     >
       <Typography
         component="h3"
@@ -52,6 +112,7 @@ const CategoryHome = () => {
             backgroundColor: "rgba(0, 0, 0, 0.04) !important",
             borderRadius: "10px",
           }}
+          ref={Itemone}
         >
           <Link to="/blogs/category/programming">
             <Typography
@@ -81,6 +142,7 @@ const CategoryHome = () => {
             backgroundColor: "rgba(0, 0, 0, 0.04) !important",
             borderRadius: "10px",
           }}
+          ref={Itemtwo}
         >
           <Link to="/blogs/category/digital-world">
             <Typography
@@ -110,6 +172,7 @@ const CategoryHome = () => {
             backgroundColor: "rgba(0, 0, 0, 0.04) !important",
             borderRadius: "10px",
           }}
+          ref={Itemthree}
         >
           <Link to="/blogs/category/technology">
             <Typography
