386f31ed3d39fab1e0e76cf5b2cf8de3273eaa39
abbas ejlali <abbas.ejlali18@gmail.com>
2023-05-29 09:10:04 -0700

    add code get data by graph ql

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,8 +1,18 @@
 import React from "react";
+
+// react router dom
 import { useParams } from "react-router-dom";
+
+// Graph Ql
+import { useQuery } from "@apollo/client";
+import { GET_POSTTOBLOG } from "../GraphQl/query";
 
 const Blog = () => {
   const { slug } = useParams();
+  const { data, loading, error } = useQuery(GET_POSTTOBLOG, {
+    variables: { slug },
+  });
+  console.log(data);
   return <div>{slug}</div>;
 };
 
